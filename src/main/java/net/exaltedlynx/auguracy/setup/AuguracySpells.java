d952104da636fb46867c96a7e1b6e95424d3e28e
ExaltedLynx <michael201103@gmail.com>
2025-04-03 01:14:30 -0400

    Completed mana bar

--- src/main/java/net/exaltedlynx/auguracy/setup/AuguracySpells.java
+++ src/main/java/net/exaltedlynx/auguracy/setup/AuguracySpells.java
@@ -29,7 +29,6 @@ public class AuguracySpells
         { name = "empty"; type = ElementType.FIRE; lvlReq = 0; manaCost = 0; }
         @Override
         protected void onCast(Player caster) {
-            Auguracy.LOGGER.atDebug().log("casted spell");
             caster.displayClientMessage(Component.literal("This is contains empty spell: Someone made an oopsie"), false);
         }
     });
